8c46ebee4b85630cc2493c868cb3234d2d3c1abb
eslowinski <158098045+eslowinski@users.noreply.github.com>
2024-04-05 21:57:48 -0500

    Update NodeFactory.java

--- NodeFactory.java
+++ NodeFactory.java
@@ -1,8 +1,8 @@
 package a5;
 
-
-
 import java.util.Random;
+
+import a5.ThreeDNode;
 
 public class NodeFactory {
 	/**
@@ -13,15 +13,15 @@ public class NodeFactory {
 	 */
 	public static ThreeDNode getThreeDNode() throws Exception {
 		Random r = new Random();
-		int z = INode.UPPER_LIMIT + r.nextInt(INode.UPPER_LIMIT-  INode.LOWER_LIMIT+1);
+		int z = INode.LOWER_LIMIT + r.nextInt(INode.UPPER_LIMIT-  INode.LOWER_LIMIT+1);
 		Node node = getNode();
 	    return new ThreeDNode(node.getX(),node.getY(),z);
 	}
 	
 	public static Node getNode() throws Exception {
 		Random r = new Random();
-		int x = INode.UPPER_LIMIT + r.nextInt(INode.UPPER_LIMIT-  INode.LOWER_LIMIT+1);
-		int y = INode.UPPER_LIMIT + r.nextInt(INode.UPPER_LIMIT-  INode.LOWER_LIMIT+1);
+		int x = INode.LOWER_LIMIT + r.nextInt(INode.UPPER_LIMIT-  INode.LOWER_LIMIT+1);
+		int y = INode.LOWER_LIMIT + r.nextInt(INode.UPPER_LIMIT-  INode.LOWER_LIMIT+1);
 		return new Node(x,y);
 	}
 }
